96b5032d346599612df13989208cd943d7277472
Lsong <song940@gmail.com>
2022-11-02 20:50:43 +0800

    update

--- detect_face_video.py
+++ detect_face_video.py
@@ -2,10 +2,14 @@ import os
 import cv2
 
 # Load the cascade
+# https://github.com/opencv/opencv/tree/master/data
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # To capture video from webcam.
-cap = cv2.VideoCapture(0) # FaceTime Camera
+cap = cv2.VideoCapture(0)  # FaceTime Camera
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
 # cap = cv2.VideoCapture("rtsp://admin:lsong940@192.168.8.22/stream1")
 
 # # Read the input image
@@ -13,9 +17,6 @@ cap = cv2.VideoCapture(0) # FaceTime Camera
 
 # # Convert into grayscale
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
 
 # Get current width of frame
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
@@ -41,12 +42,16 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
     # Saves for video
     out.write(img)
 
